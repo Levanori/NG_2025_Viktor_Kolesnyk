d0a1f4f5cfd1e04e74370acc1736817f6e1b5f58
Lenavori <kolesnykviktor2006@gmail.com>
2025-10-25 20:05:57 +0300

    Added all Task_3

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -4,6 +4,49 @@ using namespace std;
 
 int main()
 {
+    int num;
+    cout << "Enter the number of rows to create a pattern: ";
+    cin >> num;
+
+    for (int row = 0; row < num; ++row) {
+        for (int col = 0; col < num; ++col) {
+
+            int top = row;
+            int left = col;
+            int down = num-1-row;
+            int right = num-1-col;
+            int start = top;
+            bool draw_stars = false;
+
+            if (left+2 < start) {
+                start = left;
+            }
+            if (down < start) {
+                start = down;
+            }
+            if (right < start) {
+                start = right;
+            }
+
+            if (start % 2 == 0) {
+                if (row == start && col >= start-1 && col < num-start-1)
+                    draw_stars = true; // top side
+                else if (col == num - start-1 && row > start && row < num - start-1)
+                    draw_stars = true; // right side
+                else if (row == num - start-1 && col > start && col < num - start-1)
+                    draw_stars = true; // down side
+                else if (col == start && row > start && row < num - start-1)
+                    draw_stars = true; // left side
+            }
+            if(draw_stars) {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 
     int size, spaces, stars = 0;
     cout << "Enter the number of triangle rows to create a rhombus: ";
@@ -24,7 +67,7 @@ int main()
         for (int column_romb = 0; column_romb < stars; ++column_romb){
             cout << "*";
         }
-        cout << "\n";
+        cout << endl;
     }
 
     return 0;
