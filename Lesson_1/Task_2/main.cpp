1a7fd70528de7947532977e87f32b8ca6c40f1a8
Lenavori <kolesnykviktor2006@gmail.com>
2025-10-27 02:54:39 +0200

    Edited Task_2(Returned the previous code, but without copy-pasta)

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <cmath>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    float num_1, num_2, result = 0; char action = 0;
-    bool problem = false; string operation_name = " ";
+    /*float num_1, num_2, result = 0; char action = 0;
     cout << "Enter the first number: " << endl;
     cin >> num_1;
     cout << "Enter the action you want to do: +, -, *, /, ^, sqrt(enter s)" << endl;
     cin >> action;
+
     if (action == 's') {
         operation_name = "square root";
         result = sqrt(num_1);
@@ -25,11 +24,11 @@ int main()
             result = num_1+num_2;
             break;
         case '-':
-            operation_name = "the subtraction";
+            operation_name = "subtraction";
             result = num_1-num_2;
             break;
         case '*':
-            operation_name = "the multiplication";
+            operation_name = "multiplication";
             result = num_1*num_2;
             break;
         case '/':
@@ -54,6 +53,45 @@ int main()
     }
     if (!problem) {
         cout << "The result of " << operation_name << " is: " << result << endl;
+    } */
+
+    float num_1, num_2 = 0; char action = 0;
+    cout << "Enter the first number: " << endl;
+    cin >> num_1;
+    cout << "Enter the action you want to do: +, -, *, /, ^, sqrt(enter s)" << endl;
+    cin >> action;
+
+    if (action == 's') {
+        cout << "Square root = " << sqrt(num_1)  << endl;
+    }
+    else {
+        cout << "Enter the second number: " << endl;
+        cin >> num_2;
+        switch (action) {
+        case '+':
+            cout << "Addition = " << num_1+num_2  << endl;
+            break;
+        case '-':
+            cout << "Subtraction = " << num_1-num_2  << endl;
+            break;
+        case '*':
+            cout << "Multiplication = " << num_1*num_2  << endl;
+            break;
+        case '/':
+            if (num_2 == 0) {
+                cout << "You can't divide by 0!" << endl;
+            }
+            else {
+                cout << "Division = " << num_1/num_2  << endl;
+            }
+            break;
+        case '^':
+            cout << "Raising a number to a power = " << pow(num_1, num_2)  << endl;
+            break;
+        default:
+            cout << "Something wrong!" << endl;
+            break;
+        }
     }
     return 0;
 }
